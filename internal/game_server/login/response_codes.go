af56057c890e8c4a0a5df70ee445952a2383ac90
Sino <ilyas.alfan.b@gmail.com>
2018-08-02 17:54:49 +0200

    Add remaining response codes

--- internal/game_server/login/response_codes.go
+++ internal/game_server/login/response_codes.go
@@ -2,7 +2,18 @@ package login
 
 const (
 	MayProceed            = 0
+	Timeout               = 1
 	LoginSuccess          = 2
+	InvalidCredentials    = 3
+	DisabledAccount       = 4
+	AlreadyLoggedIn       = 5
+	ClientUpdated         = 6
+	WorldFull             = 7
+	LoginServerOffline    = 8
+	LimitExceeded         = 9
+	BadSessionId          = 10
 	LoginServerRejected   = 11
+	RequireMembersAccount = 12
 	CouldNotCompleteLogin = 13
+	ServerBeingUpdated    = 14
 )
