8a6088b19a23ee9f2d5fd825e0e36b03d9d2ea5e
Sino <ilyas.alfan.b@gmail.com>
2018-08-03 20:38:48 +0200

    Add item definitions loading

---
+++ internal/game_server/asset/definition/item_defs_test.go
@@ -0,0 +1,16 @@
+package definition
+
+import "testing"
+
+func TestLoadItemDefsFromFile(t *testing.T) {
+	definitions, err := LoadItemDefsFromFile("assets/definition/items.json")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	abbyWhip := definitions[4151]
+	if abbyWhip.Name != "Abyssal whip" {
+		t.Fatalf("name of item 4151 did not match 'Abyssal whip' but instead equals %v \n", abbyWhip.Name)
+	}
+}
