58b15f7fd758bbf9a5e05cf3bb4e37c19ac9e7ce
Sino <ilyas.alfan.b@gmail.com>
2018-08-03 20:55:52 +0200

    Fix Options type

--- internal/game_server/asset/definition/npc_defs.go
+++ internal/game_server/asset/definition/npc_defs.go
@@ -6,23 +6,23 @@ import (
 )
 
 type NpcDefinition struct {
-	ID               int           `json:"id"`
-	Name             string        `json:"name"`
-	Size             int           `json:"size"`
-	Stance           int           `json:"stance"`
-	Walk             int           `json:"walk"`
-	Options          []interface{} `json:"options"`
-	VisibleOnMinimap bool          `json:"visibleOnMinimap"`
-	Clickable        bool          `json:"clickable"`
-	VarbitID         int           `json:"varbitId"`
-	VarpID           int           `json:"varpId"`
-	CombatStats      []int         `json:"combatStats"`
-	CombatBonuses    []int         `json:"combatBonuses"`
-	AttackSpeed      int           `json:"attackSpeed"`
-	SlayerExpGain    float64       `json:"slayerExpGain"`
-	ImmuneToPoison   bool          `json:"immuneToPoison"`
-	ImmuneToVenom    bool          `json:"immuneToVenom"`
-	Examine          string        `json:"examine"`
+	ID               int      `json:"id"`
+	Name             string   `json:"name"`
+	Size             int      `json:"size"`
+	Stance           int      `json:"stance"`
+	Walk             int      `json:"walk"`
+	Options          []string `json:"options"`
+	VisibleOnMinimap bool     `json:"visibleOnMinimap"`
+	Clickable        bool     `json:"clickable"`
+	VarbitID         int      `json:"varbitId"`
+	VarpID           int      `json:"varpId"`
+	CombatStats      []int    `json:"combatStats"`
+	CombatBonuses    []int    `json:"combatBonuses"`
+	AttackSpeed      int      `json:"attackSpeed"`
+	SlayerExpGain    float64  `json:"slayerExpGain"`
+	ImmuneToPoison   bool     `json:"immuneToPoison"`
+	ImmuneToVenom    bool     `json:"immuneToVenom"`
+	Examine          string   `json:"examine"`
 }
 
 func LoadNpcDefsFromFile(path string) ([]NpcDefinition, error) {
