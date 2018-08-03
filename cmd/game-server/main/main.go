7e520b3622b807e409bcb20b4b0b30ed25d079d1
Sino <ilyas.alfan.b@gmail.com>
2018-08-03 21:28:37 +0200

    Add bit variable definition loading

--- cmd/game-server/main/main.go
+++ cmd/game-server/main/main.go
@@ -8,6 +8,10 @@ import (
 	"log"
 )
 
+const (
+	GamePort = 43594
+)
+
 func main() {
 	assets, err := loadAssets()
 	if err != nil {
@@ -19,7 +23,7 @@ func main() {
 	gameService := game.NewService(*assets)
 	loginService := login.NewService(gameService)
 
-	tcpServer := net.NewTcpServer(43594, loginService)
+	tcpServer := net.NewTcpServer(GamePort, loginService)
 	tcpServer.StartListening()
 }
 
@@ -49,12 +53,18 @@ func loadAssets() (*game.Assets, error) {
 		return nil, err
 	}
 
+	bitVariables, err := definition.LoadBitVariableDefsFromFile("assets/definition/bit_variables.json")
+	if err != nil {
+		return nil, err
+	}
+
 	assets := &game.Assets{
-		Items: items,
-		Npcs: npcs,
-		Objects: objects,
-		Inventories: inventories,
-		Gestures: gestures,
+		Items:        items,
+		Npcs:         npcs,
+		Objects:      objects,
+		Inventories:  inventories,
+		Gestures:     gestures,
+		BitVariables: bitVariables,
 	}
 
 	return assets, nil
@@ -66,4 +76,5 @@ func logLoadedAssets(assets game.Assets) {
 	log.Printf("Loaded %v object definitions \n", len(assets.Objects))
 	log.Printf("Loaded %v inventory definitions \n", len(assets.Inventories))
 	log.Printf("Loaded %v gesture definitions \n", len(assets.Gestures))
+	log.Printf("Loaded %v bit variable definitions \n", len(assets.BitVariables))
 }
--- internal/game_server/game/assets.go
+++ internal/game_server/game/assets.go
@@ -3,9 +3,10 @@ package game
 import "github.com/sinoz/goRS/internal/game_server/asset/definition"
 
 type Assets struct {
-	Items       []definition.ItemDefinition
-	Npcs        []definition.NpcDefinition
-	Objects     []definition.ObjectDefinition
-	Inventories []definition.InventoryDefinition
-	Gestures    []definition.GestureDefinition
+	Items        []definition.ItemDefinition
+	Npcs         []definition.NpcDefinition
+	Objects      []definition.ObjectDefinition
+	Inventories  []definition.InventoryDefinition
+	Gestures     []definition.GestureDefinition
+	BitVariables []definition.BitVariableDefinition
 }
